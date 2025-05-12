105aa3c2d59a673d6efffc1ae08b477791550507
bomerman681 <bomberman681@gmail.com>
2025-05-12 01:50:20 -0500

    Created the self.screen, and the display name.

--- src/tomogatchi.py
+++ src/tomogatchi.py
@@ -2,6 +2,10 @@ import pygame
 import sys
 import time
 
-WIDTH, HEIGHT = 600, 400
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Tamagotchi Pet")
+def __init__(self):
+    WIDTH = 500
+    HEIGHT = 500
+    self.background_color = "white"
+
+    self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Tomogatchi Pet")
